1434ae67f2e90aade0c5551b5a4d8249b169574e
jahid <jahidjob5@outlook.com>
2023-11-18 02:59:34 +0300

    SliceMethood

---
+++ slice.js
@@ -0,0 +1,16 @@
+// console.log("Hello Test Terminal");
+const bangladesh = "     Bangladesh, a land of lush greenery and vibrant culture. Home to warm hospitality and / captivating allure.A nation of resilience and spirit ever pure.";
+
+// console.log(bangladesh);
+
+parts = bangladesh.split(' ')
+sentences = bangladesh.split('/')
+
+// console.log(sentences);
+
+strip = bangladesh.slice(5, bangladesh.length,);
+// console.log(strip); 
+
+
+trimVar = bangladesh.trim()
+console.log(trimVar);
