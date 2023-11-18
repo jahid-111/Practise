d8409f4a9d78ad6503fa6f5432d4fd219c11587c
jahid <jahidjob5@outlook.com>
2023-11-18 03:31:39 +0300

    MathIn-Js

---
+++ math.js
@@ -0,0 +1,35 @@
+// console.log("Hello Terminal");
+
+const result = Math.pow(3, 8)
+// console.log(result);
+
+
+const fruit = 15;
+const banana = 30;
+
+const diff = Math.abs(fruit - banana);
+// console.log(diff);
+
+if (diff < 5) {
+    console.log("its ok");
+}
+else {
+    console.log("its not ok");
+}
+
+// ==================
+const num = 332.82988;
+
+const convert = Math.round(num)
+// console.log(convert);
+
+const convert1 = Math.ceil(num)
+// console.log(convert1);
+
+const convert2 = Math.floor(num)
+console.log(convert2);
+
+// ======================== 
+const number = 20.31;
+const out = Math.random(number);
+console.log(out);
